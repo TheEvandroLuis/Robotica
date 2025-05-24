adbaba62337fdfa7ca76c0fca9b4f46d6e676c0a
TheEvandroLuis <theevandroluisaraujo@gmail.com>
2025-05-24 15:05:02 -0300

    1 VERSAO FUNCIONAL

--- drc.py
+++ drc.py
@@ -3,16 +3,36 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
+########### MOTORES/SENSORES ###########
+hub = PrimeHub()
+motorA = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+motorB = Motor(Port.F, Direction.CLOCKWISE)
+drive_base = DriveBase(motorA, motorB, 56, 140)
+color_sensor = ColorSensor(Port.D)
+Color.BLACK = Color(270, 27, 27)
+Color.GRAY = Color(220, 30, 50)
+color_sensor.detectable_colors((Color.BLACK, Color.WHITE, Color.GRAY))
+n = 6
+tabuleiro = [[ Color.NONE for _ in range(n)] for _ in range(n)]
+linha, coluna = 0, 0
+direcao = 'E'
 
 ########### FUNCOES ###########
 def verificarFrente(): 
-    drive_base.drive(50, 0)
-    while not color_sensor.color() == (not tabuleiro[linha][coluna] ):
-        wait(0)
+    cor_atual = color_sensor.color()
+    while color_sensor.color() == cor_atual:
+        drive_base.drive(100, 0)
     drive_base.stop()
-    leitura = color_sensor.color()
-    drive_base.straight(-150)
-    return leitura
+    drive_base.straight(5)
+    if color_sensor.color()==Color.WHITE:
+        hub.light.on(Color.WHITE)
+    elif color_sensor.color()==Color.BLACK:
+        hub.light.on(Color.BLACK)
+    elif color_sensor.color()==Color.GRAY:
+        hub.light.on(Color.RED)
+    wait(500)
+    hub.light.off()
+    return color_sensor.color()
 
 def frente(linha, coluna):
     if direcao == 'D':
@@ -23,74 +43,68 @@ def frente(linha, coluna):
         linha+=1
     elif direcao == 'C':
         linha-=1
-    drive_base.straight(300)
+    drive_base.straight(150)
+    print(f"Estou em {linha}, {coluna}")
+    return linha, coluna 
 
 def virarPara(dir):
     if (dir== direcao):
-        return
+        return dir
     else: 
-        if dir =='B'
+        if dir =='B':
             if direcao == 'C':
                 drive_base.turn(180)
             elif direcao == 'D':
-                drive_base.turn(-90)
-            elif direcao == 'E'
                 drive_base.turn(90)
-        elif dir =='D'
-             if direcao == 'C':
+            elif direcao == 'E':
                 drive_base.turn(-90)
-            elif direcao == 'B':
-                drive_base.turn(90)
-            elif direcao == 'E'
-                drive_base.turn(180)
-        elif dir =='E'
-             if direcao == 'C':
+        elif dir =='D':
+            if direcao == 'C':
                 drive_base.turn(90)
             elif direcao == 'B':
                 drive_base.turn(-90)
-            elif direcao == 'E'
+            elif direcao == 'E':
                 drive_base.turn(180)
-        elif dir =='C'
-             if direcao == 'B':
+        elif dir =='E':
+            if direcao == 'C':
+                drive_base.turn(-90)
+            elif direcao == 'B':
+                drive_base.turn(90)
+            elif direcao == 'D':
+                drive_base.turn(180)
+        elif dir =='C':
+            if direcao == 'B':
                 drive_base.turn(180)
             elif direcao == 'D':
-                drive_base.turn(90)
-            elif direcao == 'E'
                 drive_base.turn(-90)
-        direcao=dir
-
-########### MOTORES/SENSORES ###########
-hub = PrimeHub()
-motorA = Motor(Port.A, Direction.CLOCKWISE)
-motorB = Motor(Port.B, Direction.CLOCKWISE)
-global drive_base = DriveBase(motor, motor_2, 56, 114)
-global color_sensor = ColorSensor(Port.C)
-global tabuleiro = [ [Color.NONE for _ in range(6)] for _ in range(6)]
-global linha = 0
-global coluna = 0
-global direcao = 'E'
+            elif direcao == 'E':
+                drive_base.turn(90)
+        return dir
 
 ########### MAIN ###########
 ########### INDO PARA O (0,0) ###########
-while verificarFrente() != Color.GRAY: ##PENSAR O QUE FAZER CASO TENHA ALGUM QUADRADO Y/G NO CAMINHO
-    frente()
+proxQuadrado = verificarFrente()
+while proxQuadrado != Color.GRAY: ##PENSAR O QUE FAZER CASO TENHA ALGUM QUADRADO Y/G NO CAMINHO
+    linha, coluna = frente(linha, coluna)
+    proxQuadrado = verificarFrente()
 
 ########### ESTOU EM (0,0) ###########
-virarPara('D')
-tabuleiro[linha][coluna] = color_sensor.color() #
+direcao = virarPara('D')
+tabuleiro[linha][coluna] = color_sensor.color()
+linha, coluna = 0, 0
 
 for i in range(6): #6 linhas
     for j in range(1, 6): #5 colunas já que estamos sempre em uma
         sensor = verificarFrente()
         if sensor!= Color.GRAY:
-            tabuleiro[linha][coluna]=sensor
-            frente
-    virarPara('B')
+            linha, coluna = frente (linha, coluna)
+            tabuleiro[linha][coluna]=color_sensor.color()
+    direcao = virarPara('B')
     sensor = verificarFrente()
-    frente()
+    linha, coluna = frente (linha, coluna)
+    tabuleiro[linha][coluna]=color_sensor.color()
     if coluna==5: #VIRA PARA O INICIO OU FINAL DO TABULEIRO
-        direcao='E'
+        direcao = virarPara('E')
     else:
-        direcao='D'
-    print(tabuleiro)
-    break
+        direcao = virarPara('D')
+print(tabuleiro)
