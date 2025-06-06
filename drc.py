7b23deedda3e83ee9e917f2200fdad9ce88cc0a9
TheEvandroLuis <theevandroluisaraujo@gmail.com>
2025-06-05 21:33:08 -0300

    OBR - Curvas nos Verdes / FIRA - Ajustando nos quadrados

--- drc.py
+++ drc.py
@@ -1,17 +1,27 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.parameters import Axis, Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
-########### MOTORES/SENSORES ###########
-hub = PrimeHub()
-motorA = Motor(Port.E, Direction.COUNTERCLOCKWISE)
-motorB = Motor(Port.F, Direction.CLOCKWISE)
-drive_base = DriveBase(motorA, motorB, 56, 140)
-color_sensor = ColorSensor(Port.D)
+hub = PrimeHub(top_side=Axis.Z, front_side=-Axis.X)
+
+########### MOTORES ###########
+motorE = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+motorD = Motor(Port.F, Direction.CLOCKWISE)
+drive_base = DriveBase(motorE, motorD, 56, 120)
+drive_base.use_gyro(True)
+
+########### SENSORES ###########
+color_sensorD = ColorSensor(Port.D)
+color_sensorE = ColorSensor(Port.C)
 Color.BLACK = Color(270, 27, 27)
 Color.GRAY = Color(220, 30, 50)
-color_sensor.detectable_colors((Color.BLACK, Color.WHITE, Color.GRAY))
+Color.MY_YELLOW = Color(67, 42, 98)
+Color.MY_GREEN = Color(138, 49, 88)
+color_sensorE.detectable_colors((Color.BLACK, Color.WHITE, Color.GRAY, Color.RED, Color.MY_YELLOW, Color.MY_GREEN))
+color_sensorD.detectable_colors((Color.BLACK, Color.WHITE, Color.GRAY, Color.RED, Color.MY_YELLOW, Color.MY_GREEN))
+
+########### VARIAVEIS ###########
 n = 6
 tabuleiro = [[ Color.NONE for _ in range(n)] for _ in range(n)]
 linha, coluna = 0, 0
@@ -19,20 +29,52 @@ direcao = 'E'
 
 ########### FUNCOES ###########
 def verificarFrente(): 
-    cor_atual = color_sensor.color()
-    while color_sensor.color() == cor_atual:
+    cor_atual = color_sensorE.color()
+    alvo = color_sensorE.reflection()
+    while color_sensorE.color() == cor_atual or color_sensorD.color() == cor_atual:
         drive_base.drive(100, 0)
     drive_base.stop()
-    drive_base.straight(5)
-    if color_sensor.color()==Color.WHITE:
+    drive_base.straight(20)
+
+    
+    if color_sensorE.color()==Color.WHITE:
         hub.light.on(Color.WHITE)
-    elif color_sensor.color()==Color.BLACK:
-        hub.light.on(Color.BLACK)
-    elif color_sensor.color()==Color.GRAY:
+        #print(f"ESQ: {color_sensorE.reflection()} | DIR: {color_sensorD.reflection()}")
+
+        while color_sensorE.color()!=Color.BLACK:
+            motorE.run(-50)
+        motorE.stop()
+
+        while color_sensorD.color()!=Color.BLACK:
+            motorD.run(-50)
+        motorD.stop()
+        
+        drive_base.straight(20)
+        #print(f"ESQ: {color_sensorE.color()} | DIR: {color_sensorD.color()}")
+    elif color_sensorE.color()==Color.BLACK:
+        hub.light.on(Color.WHITE)
+
+        while color_sensorE.color()!=Color.WHITE:
+            motorE.run(-50)
+        motorE.stop()
+
+        while color_sensorD.color()!=Color.WHITE:
+            motorD.run(-50)
+        motorD.stop()
+        
+        drive_base.straight(20)
+
+    elif color_sensorE.color()==Color.GRAY:
         hub.light.on(Color.RED)
+    elif color_sensorE.color()==Color.RED:
+        hub.light.on(Color.RED)
+    elif color_sensorE.color()==Color.MY_GREEN:
+        hub.light.on(Color.GREEN)
+    elif color_sensorE.color()==Color.MY_YELLOW:
+        hub.light.on(Color.YELLOW)
     wait(500)
     hub.light.off()
-    return color_sensor.color()
+    return color_sensorE.color()
 
 def frente(linha, coluna):
     if direcao == 'D':
@@ -43,7 +85,7 @@ def frente(linha, coluna):
         linha+=1
     elif direcao == 'C':
         linha-=1
-    drive_base.straight(150)
+    drive_base.straight(200)
     print(f"Estou em {linha}, {coluna}")
     return linha, coluna 
 
@@ -90,19 +132,20 @@ while proxQuadrado != Color.GRAY: ##PENSAR O QUE FAZER CASO TENHA ALGUM QUADRADO
 
 ########### ESTOU EM (0,0) ###########
 direcao = virarPara('D')
-tabuleiro[linha][coluna] = color_sensor.color()
+tabuleiro[linha][coluna] = color_sensorE.color()
 linha, coluna = 0, 0
 
+########### ANDANDO PELO LABIRINTO ###########
 for i in range(6): #6 linhas
     for j in range(1, 6): #5 colunas já que estamos sempre em uma
         sensor = verificarFrente()
         if sensor!= Color.GRAY:
             linha, coluna = frente (linha, coluna)
-            tabuleiro[linha][coluna]=color_sensor.color()
+            tabuleiro[linha][coluna]=color_sensorE.color()
     direcao = virarPara('B')
     sensor = verificarFrente()
     linha, coluna = frente (linha, coluna)
-    tabuleiro[linha][coluna]=color_sensor.color()
+    tabuleiro[linha][coluna]=color_sensorE.color()
     if coluna==5: #VIRA PARA O INICIO OU FINAL DO TABULEIRO
         direcao = virarPara('E')
     else:
---
+++ obr.py
@@ -0,0 +1,61 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+#####Motores######
+motorA= Motor(Port.B, Direction.COUNTERCLOCKWISE)
+motorB= Motor(Port.C, Direction.CLOCKWISE)
+drive_base= DriveBase(motorA, motorB, 56, 81)
+drive_base.use_gyro(True)
+######Variaveis#####
+Alvo=50
+Erro=0
+VelBase=75
+Kp=1
+Cor_da_vezDir= Color.WHITE
+Cor_da_vezEsq= Color.WHITE
+
+Color.BLACK=Color(220, 17, 42)
+Color.GREEN=Color(146, 73, 83)
+Color.WHITE=Color(60, 0, 99)
+
+######Sensores######
+color_sensorDir=ColorSensor(Port.D)
+color_sensorEsq=ColorSensor(Port.F)
+color_sensorMeio=ColorSensor(Port.A)
+color_sensorDir.detectable_colors((Color.BLACK, Color.WHITE, Color.GREEN))
+color_sensorEsq.detectable_colors((Color.BLACK, Color.WHITE, Color.GREEN))
+color_sensorMeio.detectable_colors((Color.BLACK, Color.WHITE, Color.GREEN))
+
+#######Funcoes######
+def seguirLinha():
+    Erro= color_sensorMeio.reflection() - Alvo
+    Erro= Kp*Erro
+    drive_base.drive(VelBase, Erro)
+
+def virarEsq():
+    drive_base.straight(80)
+    drive_base.turn(-95)
+
+def virarDir():
+    drive_base.straight(80)
+    drive_base.turn(95)
+
+#######Main#######
+
+while True:
+    Cor_da_vezDir= color_sensorDir.color()
+    Cor_da_vezEsq= color_sensorEsq.color()
+    print(f"E: {color_sensorEsq.color()} | M: {color_sensorMeio.color()} | D: {color_sensorDir.color()}")
+    if Cor_da_vezDir==Color.GREEN and Cor_da_vezEsq==Color.GREEN:
+        drive_base.turn(180)
+    elif Cor_da_vezEsq==Color.GREEN:
+        virarEsq()
+    elif Cor_da_vezDir==Color.GREEN:
+        virarDir()
+    else:
+        seguirLinha()
+
